18501c2f0de845974802535aa5fd10c1d324f287
Pavel Velichko <krasniyvoron@rambler.ru>
2025-03-17 11:45:24 +0300

    обновил тесты

--- tests.py
+++ tests.py
@@ -16,12 +16,8 @@ class TestBooksCollector:
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниж
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    import pytest
+    from main import BooksCollector
 
     class TestBooksCollector:
 
@@ -32,6 +28,12 @@ class TestBooksCollector:
             assert 'Гарри Поттер' in collector.get_books_genre()
 
         @pytest.mark.parametrize("book_name", ["", "А" * 41])
+        def test_add_new_book_invalid_name(self, book_name):
+            collector = BooksCollector()
+            collector.add_new_book(book_name)
+            assert len(collector.get_books_genre()) == 0  # Книга не должна добавляться
+
+    @pytest.mark.parametrize("book_name", ["", "А" * 41])
         def test_add_new_book_invalid_name(self, book_name):
             collector = BooksCollector()
             collector.add_new_book(book_name)
